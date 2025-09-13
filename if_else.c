3314c4717bc4757066702d6780e21dccef60343c
Jasleen74 <preetjee@gmail.com>
2025-09-13 20:17:24 +0530

    (this is if else statement

---
+++ if_else.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+     
+    int a ;
+    printf ("enter ur number : ");
+    scanf ("%d", &a);
+    
+    if (a <20)
+    {
+        printf(" the value is true");
+    }
+
+    else {
+        
+        printf("the value is not true");
+    } ;
+ return 0;
+}

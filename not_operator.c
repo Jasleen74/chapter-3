c5ffa2f6a391c64e6706bc1df868248f69a6296d
Jasleen74 <preetjee@gmail.com>
2025-09-18 10:35:44 +0530

    (NOT logical operator)

---
+++ not_operator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+     int a ;
+     printf ("put the value of a either 0 or 1 : ");
+     scanf("%d",&a);
+
+     printf ("the value of not (a) is %d", !a );
+     
+ return 0;
+}

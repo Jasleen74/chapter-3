ca2004ba22e6e020988dab6120e4096f229e6158
Jasleen74 <preetjee@gmail.com>
2025-09-25 11:17:43 +0530

    (the use of ternary operators )

---
+++ ternary.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+     
+    int a , b ;
+
+    printf("put the value of a : ");
+    scanf("%d",&a);
+
+
+    printf("put the value of b : ");
+    scanf("%d",&b);
+
+
+    a>b ? printf("a is greater than b") : printf("b is greater than a ");
+
+// ? this stands for if  and : this stands for else
+
+
+  
+ return 0;
+}

143b4c1667b5a56a9cd3e80c8c3542296e03d500
Jasleen74 <preetjee@gmail.com>
2025-09-30 20:45:26 +0530

    (the use of ascii values for checking the upper case and lower case alphabets)

--- ques_5.c
+++ ques_5.c
@@ -1,6 +1,33 @@
 #include<stdio.h>
 int main() {
-    char ;
+    char ch ;
+    printf("Enter ur chracter : ");
+    scanf("%c",&ch);
+
+
+    printf("The character is %c\n",ch);
+    printf("The ascii value of the chracter  is %d \n",ch);
+
+    // here the %d gives the ascii value of the character
+    // ascii vlaue is specific code assigned to the chrcater etc etc
+
+
+    if (ch >= 65 && ch<= 90){
+        printf("The letter is an upper case ");
+    }
+
+    else if (ch >= 97 && ch <= 122){
+        printf("The letter is a lower case ");
+
+    }
+
+    else{
+       printf("THE VALUE U ENTERED IS NOT AN ALPHABET :-(");
+
+    }
+
+    
+
      
 
   
